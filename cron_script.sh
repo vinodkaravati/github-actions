1c5bd082f76e3fdf9881a5b1ed2c55a5db3f3638
vinodkaravati <40589609+vinodkaravati@users.noreply.github.com>
2025-09-17 15:40:32 +0530

    Create cron_script.sh

---
+++ cron_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Get current date and time in YYYY-MM-DD_HH-MM-SS format
+timestamp=$(date +"%F_%H-%M-%S")
+# Define base filename
+base_name="Cron_Report"
+# Create final filename
+filename="${base_name}_${timestamp}.txt"
+# Create the file (or write to it)
+echo "Generated at $timestamp" > "$filename"
